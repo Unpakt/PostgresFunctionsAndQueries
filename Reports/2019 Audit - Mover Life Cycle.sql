3f97df7c9896c175f6a79d88be111d6eab529cce
Amery Martinat <amery@polite.io>
2019-04-01 14:45:41 -0400

    new mover cut calculation

--- Reports/2019 Audit - Mover Life Cycle.sql
+++ Reports/2019 Audit - Mover Life Cycle.sql
@@ -1,4 +1,21 @@
-SELECT
+DROP FUNCTION IF EXISTS mover_life_cycle(DATE, DATE);
+CREATE OR REPLACE FUNCTION mover_life_cycle(start_date DATE, end_date DATE)
+RETURNS TABLE(
+  "Status" TEXT,
+  "# of plans difference" BIGINT,
+  "Change in Value" NUMERIC,
+  "Change in Amount Due to movers" NUMERIC,
+  "# of plans at start date" BIGINT,
+  "# of plans at end date" BIGINT,
+  "Value at start date" NUMERIC,
+	"Value at end date" NUMERIC,
+	"Mover Cut at start Date" NUMERIC,
+	"Mover Cut at end Date" NUMERIC
+	) AS
+$func$
+BEGIN
+RETURN QUERY
+(SELECT
 	coalesce(first."Status",second."Status") as "Status",+
 	coalesce(second.num_plans,0) - coalesce(first.num_plans,0) as "# of plans difference",
 	coalesce(second.total,0.00) - coalesce(first.total,0.00) as "Change in Value",
@@ -13,15 +30,20 @@ SELECT
 	(SELECT
 	count(*) num_plans,
 	round(sum(total),2) total,
-	sum(mover_cut) mover_cut,
+	sum(total*(1-.01*estimate_commission_rate))  mover_cut,
 	max(move_date) most_recent_move_date,
 	min(move_date) oldest_move_date,
-	CASE WHEN (SELECT count(*) from payment_entries WHERE chargeable_id = move_plans.id AND chargeable_type = 'MovePlan' AND updated_at::DATE <= '2019-03-11' AND updated_at > created_at AND payment_id is not null GROUP BY chargeable_id)  > 0 THEN '5-paid'
-		WHEN (SELECT count(*) from payment_entries WHERE chargeable_id = move_plans.id AND chargeable_type = 'MovePlan' AND created_at::DATE <= '2019-03-11' AND payment_id is null GROUP BY chargeable_id) > 0 THEN '4b-verified'
-		WHEN move_date::DATE  <= ('2019-03-11'::DATE ) THEN '4a-completed'
-		WHEN move_date::DATE  <= ('2019-03-11'::DATE + 2) THEN '3-charged'
-		WHEN jobs.created_at::DATE  <= ('2019-03-11'::DATE ) THEN '2-pending_at_date'
-		WHEN jobs.created_at::DATE  > ('2019-03-11'::DATE ) THEN '1-future_pending'
+	CASE
+		WHEN (SELECT count(*) from payment_entries
+		  WHERE chargeable_id = move_plans.id
+			AND chargeable_type = 'MovePlan'
+			AND updated_at::DATE <= start_date AND updated_at > created_at
+			AND payment_id is not null GROUP BY chargeable_id)  > 0
+	  THEN '5-paid'
+		WHEN move_date::DATE  <= start_date  THEN '4-completed'
+		WHEN move_date::DATE  <= (start_date + 2) THEN '3-charged'
+		WHEN jobs.created_at::DATE  <= start_date THEN '2-pending_at_date'
+		WHEN jobs.created_at::DATE  > start_date THEN '1-future_pending'
 	END as "Status"
 	FROM jobs
 	JOIN move_plans on move_plans.id = jobs.move_plan_id And mover_state not in ('declined','cancelled_pending','cancelled_acknowledged','new') AND user_state not in ( 'reserved_cancelled','reserved','cancelled')
@@ -32,21 +54,35 @@ SELECT
   (SELECT
 	count(*) num_plans,
 	round(sum(total),2) total,
-	sum(mover_cut) mover_cut,
+	sum(total*(1-.01*estimate_commission_rate)) mover_cut,
 	max(move_date) most_recent_move_date,
 	min(move_date) oldest_move_date,
-	CASE WHEN (SELECT count(*) from payment_entries WHERE chargeable_id = move_plans.id AND chargeable_type = 'MovePlan' AND updated_at::DATE <= '2019-03-25' AND updated_at > created_at AND payment_id is not null GROUP BY chargeable_id) > 0 THEN '5-paid'
-		WHEN (SELECT count(*) from payment_entries WHERE chargeable_id = move_plans.id AND chargeable_type = 'MovePlan' AND created_at::DATE <= '2019-03-25' AND payment_id is null GROUP BY chargeable_id) > 0 THEN '4b-verified'
-		WHEN move_date::DATE  <= ('2019-03-25'::DATE ) THEN '4a-completed'
-		WHEN move_date::DATE  <= ('2019-03-25'::DATE + 2) THEN '3-charged'
-		WHEN jobs.created_at::DATE  <= ('2019-03-25'::DATE ) THEN '2-pending_at_date'
-		WHEN jobs.created_at::DATE  > ('2019-03-25'::DATE ) THEN '1-future_pending'
+	CASE
+		WHEN (SELECT count(*) from payment_entries
+			WHERE chargeable_id = move_plans.id
+			AND chargeable_type = 'MovePlan'
+			AND updated_at::DATE <= end_date AND updated_at > created_at
+			AND payment_id is not null GROUP BY chargeable_id) > 0
+	  THEN '5-paid'
+		WHEN move_date::DATE  <= end_date THEN '4-completed'
+		WHEN move_date::DATE  <= (end_date + 2) THEN '3-charged'
+		WHEN jobs.created_at::DATE  <= end_date THEN '2-pending_at_date'
+		WHEN jobs.created_at::DATE  > end_date THEN '1-future_pending'
 	END as "Status"
 	FROM jobs
 	JOIN move_plans on move_plans.id = jobs.move_plan_id  And mover_state not in ('declined','cancelled_pending','cancelled_acknowledged','new') AND user_state not in ( 'reserved_cancelled','reserved','cancelled')
 	JOIN estimates on estimates.move_plan_id = move_plans.id
 	JOIN uuids on uuids.uuidable_id = move_plans.id AND uuids.uuidable_type = 'MovePlan'
-	GROUP BY "Status") as second on first."Status" = second."Status";
+	GROUP BY "Status") as second on first."Status" = second."Status");
+	END
+$func$ LANGUAGE plpgsql;
+
+SELECT * FROM mover_life_cycle('2019-03-11'::DATE,'2019-03-25'::DATE);
+
+SELECT(5344393.9 - 2760272.56);
 
 
-SELECT distinct user_state FROM jobs;
+SELECT distinct estimate_commission_rate FROM move_plans;
+
+
+SELECT * FROM payment_entries WHERE payment_id IS NULL;
